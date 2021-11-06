c34e698f8912a1c3a93794c9dae4b02e0377dcec
Сергей Куцев <sergeikutsev@yandex.ru>
2021-11-06 13:36:30 +0300

    Добавьте файлы проекта.

---
+++ Homework23/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework23
+{
+    // Разработать асинхронный метод для вычисления факториала числа. В методе Main выполнить проверку работы метода.
+    class Program
+    {
+        static int GetFactorial(int n)
+        {
+            int factorial = 1;
+            for (int i = 2; i <= n; i++)
+                factorial *= i;
+            return factorial;
+        }
+
+        static async Task<int> GetFactorialAsync(int n)
+        {
+            Console.WriteLine($"Факториал числа {n} вычисляется.");
+            int Result = await Task.Run(() => GetFactorial(n));
+            Console.WriteLine($"Расчет окончен.");
+            return Result;
+        }
+
+        static void Main(string[] args)
+        {
+            int result = GetFactorialAsync(10).Result;
+            Console.WriteLine(result); ;
+            Console.ReadKey();
+        }
+    }
+}
